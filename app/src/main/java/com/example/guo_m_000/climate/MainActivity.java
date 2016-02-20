05df786cdb914726b46a39ea26fa665467fa2c22
ming yang <ming@ming.email>
2016-02-20 02:24:15 -0600

    init android

---
+++ app/src/main/java/com/example/guo_m_000/climate/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.guo_m_000.climate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.climate.login.LoginButton;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login);
+        loginButton.setCredentials("dpitl9gm6s321m", "lcik8jaau3bimv6rhdllufv4qt");
+
+        loginButton.registerListener(new LoginButton.LoginListener() {
+            @Override
+            public void onLogin(JSONObject jsonObject) {
+                Log.d("login", "Logged in");
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.d("login", e.toString());
+            }
+        });
+
+    }
+}
